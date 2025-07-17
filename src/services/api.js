cceba68d9d5f9af701e4dc10284bb738edabd258
Vitor Fregulia <vitorfregulia2005@gmail.com>
2025-07-17 00:20:11 -0300

    FRONTEND

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api-vitor-fregulia-0a4f8e09417c.herokuapp.com/';
 
 // Configuração do Axios
 const api = axios.create({
